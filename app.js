bc9fa8cd320eb426a31a97d1fe119c9a1b8fbb89
bbayne1979 <bbayne1979@yahoo.com>
2026-07-17 11:06:53 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// v2.0 scoring engine will be implemented next.
+const players=['Player 1','Player 2','Player 3','Player 4'];
+const scores=[0,0,0,0];
+let wolf=0;
+const board=document.getElementById('board');
+const partners=document.getElementById('partners');
+function drawBoard(){
+ board.innerHTML='';
+ players.map((p,i)=>({p,s:scores[i]}))
+ .sort((a,b)=>b.s-a.s)
+ .forEach(r=>{
+   board.innerHTML+=`<tr><td>${r.p}</td><td>${r.s}</td></tr>`;
+ });
+ document.getElementById('wolf').textContent=players[wolf];
+}
+function drawPartners(){
+ partners.innerHTML='';
+ players.forEach((p,i)=>{
+   if(i===wolf)return;
+   const b=document.createElement('button');
+   b.textContent=p;
+   partners.appendChild(b);
+ });
+}
+drawBoard();
+drawPartners();
